529e1c4076ec7d94cabe340a7e4a2226eb5b0552
rahul_dondeti <rahulnani2@gmail.com>
2024-07-03 20:24:26 +0530

    Update helloWorld.py
    Created a New directory Day-01 and copied content of helloWorld.py to a new file inside Day-01 Directory

--- Day01-Introduction/helloWorld.py
+++ Day01-Introduction/helloWorld.py
@@ -1,1 +1,18 @@
-
+# Day-01 30 Days of Python Challenge
+# Introduction 
+print("Hello World ! ")
+# Arithmetic Operators in Pythin 
+print(2 + 3) # addition 
+print(3 - 2) # Subtraction 
+print(100 * 500)  # Multiplication 
+print (100 //10 ) #floor Division 
+print (100 /10 )  # Division 
+print (21 % 4)    # Modulus
+# Checking DataTypes 
+print(type(10))    # Integer
+print(type(3.14))  # Float
+print(type("Python")) #String  
+print (type([1,2,3])) #list
+print(type({'language' : 'python'}))  #Dictionary
+print(type({9.8,3.14,2.7}))  #SET
+print(type((9.8,3.14,2.7)))  #Tuple

120b2c4d7e0518e3c9f469558cad81918498d9d4
rahulnani2 <rahulnani2@gmail.com>
2024-11-08 10:01:38 -0500

    Updated function in OS-listFiles.py

--- OS-listFiles.py
+++ OS-listFiles.py
@@ -1,16 +1,19 @@
 import os
+
+def list_files(folders_list):
+     for folder in folders_list:
+        try:
+            files = os.listdir(folders_list)
+        except FileNotFoundError:
+            print("Directory not found ! Enter a valid dir " + folder)
+            continue
+        except PermissionError:
+            print("Permission denied to fodler " +  folder )
+            continue
+        for file in files:
+            print(file)
+ 
+
 folderList = input("""Enter the list of directories to list 
                    the files with spaces in between each dir""").split()
-
-for folder in folderList:
-    try:
-        files = os.listdir(folderList)
-
-    except FileNotFoundError:
-        print("Input folder is Invalid :" , folder)
-        continue
-    except PermissionError:
-        print("Permission Denied for the folder :", folder)
-        continue
-    for file in files:
-        print(file)
+list_files(folderList)
